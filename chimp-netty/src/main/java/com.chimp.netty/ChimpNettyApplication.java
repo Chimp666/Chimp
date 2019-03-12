0e4f68cbd20407dc49177dc7cdc2623617b94818
Chimp <157795195@qq.com>
2019-03-12 22:01:12 +0800

    init

---
+++ chimp-netty/src/main/java/com.chimp.netty/ChimpNettyApplication.java
@@ -0,0 +1,13 @@
+package com.chimp.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimpNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChimpNettyApplication.class, args);
+    }
+
+}
---
+++ chimp-netty/src/test/java/chimp/chimpnetty/ChimpNettyApplicationTests.java
@@ -0,0 +1,16 @@
+package chimp.chimpnetty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChimpNettyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
