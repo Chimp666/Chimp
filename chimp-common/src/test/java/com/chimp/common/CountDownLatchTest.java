468ab335a39ec2b14ccf8383c4dbcfbffc9e3e46
Chimp <157795195@qq.com>
2019-03-13 00:48:33 +0800

    countDownLatch & cyclicBarrier &semaphore

---
+++ chimp-common/src/test/java/com/chimp/common/CountDownLatchTest.java
@@ -0,0 +1,51 @@
+package com.chimp.common;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchTest {
+
+    public static void main(String[] args){
+
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    System.out.println("线程:" + Thread.currentThread().getName() + "正在运行！");
+                    Thread.sleep(3000);
+                    System.out.println("线程:" + Thread.currentThread().getName() + "结束！");
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    System.out.println("线程:" + Thread.currentThread().getName() + "正在运行！");
+                    Thread.sleep(3000);
+                    System.out.println("线程:" + Thread.currentThread().getName() + "结束！");
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        try {
+            System.out.println("等待2个子线程执行完毕...");
+            countDownLatch.await();
+            System.out.println("2个子线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
