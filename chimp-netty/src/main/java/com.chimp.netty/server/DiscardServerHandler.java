0f7321a3f6cd88abc142639384354a807b2e4b34
Chimp <157795195@qq.com>
2019-06-04 23:30:24 +0800

    netty

---
+++ chimp-netty/src/main/java/com.chimp.netty/server/DiscardServerHandler.java
@@ -0,0 +1,40 @@
+package com.chimp.netty.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 处理服务端 channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // 默默地丢弃收到的数据
+        System.out.println("丢弃...");
+//        ByteBuf in = (ByteBuf) msg;
+//        try {
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+//        } finally {
+//            ReferenceCountUtil.release(msg); // (2)
+//        }
+
+//        ((ByteBuf) msg).release(); // (3)
+
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        System.out.println("关闭连接...");
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ chimp-webapp/src/main/java/com/chimp/web/ChimpWebappApplication.java
@@ -0,0 +1,13 @@
+package com.chimp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimpWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChimpWebappApplication.class, args);
+    }
+
+}
