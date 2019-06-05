ddbe067ed822b060c1f7fe300a1946d11d50b32a
Chimp <157795195@qq.com>
2019-06-06 00:07:24 +0800

    spring cloud

--- chimp-webapp/src/main/java/com/chimp/web/ChimpWebappApplication.java
+++ chimp-webapp/src/main/java/com/chimp/web/ChimpWebappApplication.java
@@ -2,12 +2,23 @@ package com.chimp.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ChimpWebappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChimpWebappApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- chimp-webapp/src/main/java/com/chimp/web/controller/TestController.java
+++ chimp-webapp/src/main/java/com/chimp/web/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.chimp.web.controller;
 
+import com.chimp.web.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    HelloService helloService;
+
     @GetMapping("/api/test")
     public String test(){
-        return "hello world";
+        return helloService.hiService("liuxing");
     }
 
 }
